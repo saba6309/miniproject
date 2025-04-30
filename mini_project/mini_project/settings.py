12039c533fe07b6247e95b13eee3afdfa3fc363e
saba6309 <shafiasaba6309@gmail.com>
2025-05-01 00:04:20 +0530

    Update settings.py

--- mini_project/mini_project/settings.py
+++ mini_project/mini_project/settings.py
@@ -152,7 +152,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-GENERATIVE_AI_KEY = os.environ.get('GENERATIVE_AI_KEY','AIzaSyCzY9Ppuvm_cSYa1P5BA8gc8GaWqeUuhXc') 
+GENERATIVE_AI_KEY = os.environ.get('GENERATIVE_AI_KEY','#your API key') 
 
 if not GENERATIVE_AI_KEY:
     raise ValueError('GENERATIVE_AI_KEY environment variable not set')
